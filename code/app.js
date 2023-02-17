f028f36a97f708326efebaac61856e2a521ce046
CHANDRA CHURD BHARATI <110090268+chandrachurd2004@users.noreply.github.com>
2023-02-17 07:09:05 +0530

    added more feature to code

--- code/app.js
+++ code/app.js
@@ -1,19 +1,44 @@
 const about = document.querySelector(".about");
 const btns = document.querySelectorAll(".tab-btn");
 const articles = document.querySelectorAll(".content");
+const items = [...document.querySelectorAll('.number')];
+
 about.addEventListener("click", function (e) {
   const id = e.target.dataset.id;
   if (id) {
-    // remove selected from other buttons
+    
     btns.forEach(function (btn) {
       btn.classList.remove("active");
     });
     e.target.classList.add("active");
-    // hide other articles
+    
     articles.forEach(function (article) {
       article.classList.remove("active");
     });
     const element = document.getElementById(id);
     element.classList.add("active");
   }
+});
+const updateCount = (el) => {
+  const value = parseInt(el.dataset.value);
+  const increment = Math.ceil(value / 100000);
+  // const increment = 1;
+  let initialValue = 0;
+
+  const increaseCount = setInterval(() => {
+    initialValue += increment;
+
+    if (initialValue > value) {
+      el.textContent = `${value}+`;
+      clearInterval(increaseCount);
+      return;
+    }
+
+    el.textContent = `${initialValue}+`;
+  }, 1);
+  // console.log(increaseCount);
+};
+
+items.forEach((item) => {
+  updateCount(item);
 });
